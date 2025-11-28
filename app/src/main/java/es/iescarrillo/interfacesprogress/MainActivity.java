3f47ffe9556962acc1d13727cb8d9aaad03540fa
Cristian <cleobla0802@iescarrillo.es>
2025-11-28 10:36:04 +0100

    Tengo que arreglar el color de los cuadrados de los textos en la pagina principal

--- app/src/main/java/es/iescarrillo/interfacesprogress/MainActivity.java
+++ app/src/main/java/es/iescarrillo/interfacesprogress/MainActivity.java
@@ -7,10 +7,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnAcceder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAcceder = findViewById(R.id.button);
+
+        btnAcceder.setOnClickListener(v -> {
+            Intent i = new Intent(this, SecondActivity.class);
+            startActivity(i);
+        });
 
     }
 }
